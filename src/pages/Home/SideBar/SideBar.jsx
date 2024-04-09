4fc63896ae05471c2aa64293026ebc9f36be4a03
doankietdev <doankietdev@gmail.com>
2024-04-09 23:39:42 +0700

    fix sidebar

--- src/pages/Home/SideBar/SideBar.jsx
+++ src/pages/Home/SideBar/SideBar.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { FaListUl } from 'react-icons/fa6'
 import { routesConfig } from '~/config'
 import { getCategories } from '~/services/categoriesServices'
 
@@ -16,15 +17,21 @@ function SideBar() {
 
   return (
     <div className='col-span-3 border h-fit max-h-full overflow-auto'>
-      {categories?.map((category) => (
-        <Link
-          key={category._id}
-          to={routesConfig.productsOfCategory(category.slug)}
-          className='block w-full px-[20px] py-[15px] text-[14px] hover:text-main capitalize'
-        >
-          {category.title}
-        </Link>
-      ))}
+      <div className='px-[20px] py-[15px] flex gap-2 items-center bg-main text-white font-semibold'>
+        <FaListUl size='20px' />
+        <span>All Categories</span>
+      </div>
+      <div>
+        {categories?.map((category) => (
+          <Link
+            key={category._id}
+            to={routesConfig.productsOfCategory(category.slug)}
+            className='block w-full px-[20px] py-[15px] text-[14px] hover:text-main capitalize'
+          >
+            {category.title}
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
