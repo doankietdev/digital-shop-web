80c5f7e371df93ff9d611c15867396d6585c16b6
doankietdev <doankietdev@gmail.com>
2024-10-20 20:53:08 +0700

    fix security tag

---
+++ src/apis/vi/sessionApis.js
@@ -0,0 +1,7 @@
+const getLoginSessionForCurrentApi = '/v1/sessions/get-for-current'
+const logoutSessionForCurrentApi = '/v1/sessions/logout-session-for-current'
+
+export {
+  getLoginSessionForCurrentApi,
+  logoutSessionForCurrentApi
+}
